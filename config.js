befe22627ac6f77949db4f8df0c265ed400e850c
edwardoboh <obohedward@gmail.com>
2022-09-12 01:20:17 +0100

    chore: update contract address

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
       module.exports = {
-        contractAddress: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
+        contractAddress: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
         ipfsInfuraId: "29FLjpPP09BaPOk3eefbPjEin04",
         ipfsInfuraSecret: "a12fddffca353429eb0375157d997c50",
       }
